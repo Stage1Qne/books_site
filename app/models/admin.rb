0e35533faeb83310348a3a2e8833fc080fa67547
stage1qne <stage1qne@gmail.com>
2014-06-07 15:14:02 +0300

    change Admin model devise helpers

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,5 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :trackable, :timeoutable, :rememberable, :recoverable, :validatable
+  devise :database_authenticatable, :timeoutable, :rememberable, :validatable
 end
