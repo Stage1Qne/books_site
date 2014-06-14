15514aeea0a728c1ce86b59dd6c5e8e8379af07f
stage1qne <stage1qne@gmail.com>
2014-06-14 18:29:52 +0300

    front end Comments functional

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
+  default_scope { order("id ASC") }
   validates :email, :full_name, presence: true
   validates :content, presence: true, length: { maximum: 2000 }
 
